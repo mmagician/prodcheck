6366c2eb31ff3219547d1a59ce965bac8786f5c0
Marcin <marcin.gorny.94@protonmail.com>
2022-10-26 13:25:37 +0200

    run benchmarks on a range of degrees

--- benches/util_benches.rs
+++ benches/util_benches.rs
@@ -2,21 +2,34 @@ use ark_poly::{DenseMultilinearExtension, MultilinearExtension};
 use ark_prodcheck::ml_prodcheck::{compute_f, compute_xy_vectors_from_x};
 use ark_std::test_rng;
 use ark_test_curves::bls12_381::{Bls12_381, Fr};
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn bench_xy(c: &mut Criterion) {
     let mut rng = test_rng();
-    let v = DenseMultilinearExtension::<Fr>::rand(10, &mut rng);
+    let mut group = c.benchmark_group("compute xy");
     let x = vec![0u8, 0u8, 0u8, 0u8, 0u8];
 
-    c.bench_function("compute xy, l: 5", |b| {
-        b.iter(|| compute_xy_vectors_from_x::<Bls12_381>(x.clone(), 5, &v))
-    });
-    c.bench_function("compute xy, l: 10", |b| {
-        b.iter(|| compute_xy_vectors_from_x::<Bls12_381>(vec![], 10, &v))
-    });
-    c.bench_function("compute f", |b| b.iter(|| compute_f::<Bls12_381>(&v)));
+    for m in [5usize, 10usize, 20usize, 25usize].iter() {
+        let v = DenseMultilinearExtension::<Fr>::rand(*m, &mut rng);
+        group.bench_with_input(BenchmarkId::from_parameter(m), m, |b, &m| {
+            b.iter(|| compute_xy_vectors_from_x::<Bls12_381>(x.clone(), m - x.len(), &v));
+        });
+    }
+    group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn bench_f(c: &mut Criterion) {
+    let mut rng = test_rng();
+    let mut group = c.benchmark_group("compute f");
+
+    for m in [5usize, 10usize, 15usize].iter() {
+        let v = DenseMultilinearExtension::<Fr>::rand(*m, &mut rng);
+        group.bench_with_input(BenchmarkId::from_parameter(m), m, |b, _| {
+            b.iter(|| compute_f::<Bls12_381>(&v));
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, bench_xy, bench_f);
 criterion_main!(benches);
