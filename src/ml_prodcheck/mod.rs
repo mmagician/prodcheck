a0da04f44ca4817b79acc1f7005c5fc8ca8f4a70
Marcin <marcin.gorny.94@protonmail.com>
2022-10-27 16:28:30 +0200

    compute g working

--- src/ml_prodcheck/mod.rs
+++ src/ml_prodcheck/mod.rs
@@ -9,8 +9,8 @@ use ark_linear_sumcheck::{
     rng::{Blake2s512Rng, FeedableRNG},
     Error,
 };
-use ark_poly::{DenseMultilinearExtension, MultilinearExtension};
-use ark_std::{marker::PhantomData, rc::Rc, vec::Vec, One, Zero};
+use ark_poly::{polynomial, DenseMultilinearExtension, MultilinearExtension};
+use ark_std::{marker::PhantomData, rc::Rc, vec::Vec, One, UniformRand, Zero};
 
 pub struct MLProdcheck<E: Pairing>(#[doc(hidden)] PhantomData<E>);
 
@@ -31,27 +31,29 @@ impl<E: Pairing> MLProdcheck<E> {
         // 1. Commit to v
         let comm_v = E::G1::default();
 
+        // 2. Compute f from v
+        let (f, p) = compute_f::<E>(v);
+
         // 3. Commit to f
         let comm_f = E::G1::default();
 
-        // 4. Compute MLE g of f:
-        let (g, p) = compute_f::<E>(v);
+        // 4. use the FS randomness to sample a random tau
+        let tau: Vec<E::ScalarField> = (0..v.num_vars())
+            .map(|_| E::ScalarField::rand(&mut fs_rng))
+            .collect();
 
-        // 6. use the FS randomness to sample a random gamma
+        // 5. Compute MLE g of f:
+        //  G(x, t) = eq(t, x) * f(x,0)*f(x,1) - f(1,x)
+        // g(t) = \sum_{x \in {0,1}^n} G(x, t)
+        let g = compute_G::<E>(&f, &tau);
+        let polynomial = ListOfProductsOfPolynomials::new(v.num_vars());
 
         // 7. Provide openings and proofs of f(0, gamma) = g(gamma)
 
         // 8. Provide openings and proofs of f(1, ..., 1, 0) = P
 
         // 5. Run sum check protocol on g
-        let mut polynomial = ListOfProductsOfPolynomials::new(g.num_vars());
-        polynomial.add_product(
-            vec![Rc::new(DenseMultilinearExtension::from_evaluations_vec(
-                v.num_vars() + 1,
-                vec![E::ScalarField::zero()],
-            ))],
-            E::ScalarField::one(),
-        );
+
         let mut prover_state = IPForMLSumcheck::prover_init(&polynomial);
         let mut verifier_msg = None;
         let mut prover_msgs = Vec::with_capacity(polynomial.num_variables);
@@ -126,14 +128,82 @@ pub fn compute_f<E: Pairing>(
     (f, p)
 }
 
+// Compute G_t(x) = eq(x,t) * (f(1,x) - f(x,0)*f(x,1))
+#[allow(non_snake_case)]
+pub fn compute_G<E: Pairing>(
+    f: &DenseMultilinearExtension<E::ScalarField>,
+    t: &Vec<E::ScalarField>,
+) -> DenseMultilinearExtension<E::ScalarField> {
+    let s = f.num_vars() - 1;
+    let mut fs_evals = vec![E::ScalarField::zero(); 1 << (s)];
+    let eq = compute_mle_eq::<E>(t, s);
+
+    for b_x in 0usize..(1 << s) {
+        let (v_index, _) = b_x.reverse_bits().overflowing_shr(usize::BITS - (s as u32));
+
+        println!("b_x: {}", b_x);
+        let f_index = (v_index << 1) + 1;
+        println!("f_index: {}", f_index);
+
+        let f_index_l = v_index;
+        println!("f_index_l: {}", f_index_l);
+        let f_index_r = v_index + (1 << s);
+        println!("f_index_r: {}", f_index_r);
+
+        fs_evals[v_index] = eq[v_index]
+            * (f.evaluations[f_index] - f.evaluations[f_index_l] * &f.evaluations[f_index_r]);
+    }
+    // fs = f(1,x)) - f(x,0)*f(x,1)
+    let fs = DenseMultilinearExtension::from_evaluations_vec(s, fs_evals);
+    // G_t(x) = eq(t, x) * fs(x)
+    // let mut polynomial = ListOfProductsOfPolynomials::new(s);
+    // polynomial.add_product(vec![Rc::new(eq), Rc::new(fs)], E::ScalarField::one());
+
+    // polynomial
+    fs
+}
+
+fn compute_mle_eq<E: Pairing>(
+    t: &Vec<E::ScalarField>,
+    s: usize,
+) -> DenseMultilinearExtension<E::ScalarField> {
+    let mut eq_evals = vec![E::ScalarField::one(); 1 << s];
+    for b_x in 0usize..(1 << s) {
+        // turn b_x into a vector of field elements, where each element is F::zero() or F::one()
+        let v_field: Vec<E::ScalarField> = (0..s)
+            .map(|i| {
+                if (b_x >> i) & 1 == 1 {
+                    E::ScalarField::one()
+                } else {
+                    E::ScalarField::zero()
+                }
+            })
+            .collect();
+
+        // eq_i = \prod_{i=1}^s (t_i*x_i + (1-t_i)*(1-x_i))
+        let eq_i: E::ScalarField = t
+            .iter()
+            .enumerate()
+            .map(|(i, t_i)| {
+                *t_i * &v_field[i]
+                    + (E::ScalarField::one() - t_i) * (E::ScalarField::one() - v_field[i])
+            })
+            .product();
+        eq_evals[b_x] = eq_i;
+    }
+    DenseMultilinearExtension::from_evaluations_vec(s, eq_evals)
+}
+
 #[cfg(test)]
 mod tests {
     use ark_poly::{DenseMultilinearExtension, MultilinearExtension};
     use ark_std::test_rng;
     use ark_test_curves::bls12_381::{Bls12_381, Fr};
 
-    use crate::ml_prodcheck::compute_f;
+    use crate::ml_prodcheck::{compute_f, compute_mle_eq};
     use ark_std::{One, UniformRand, Zero};
+
+    use super::compute_G;
 
     #[test]
     fn f_computed_correctly() {
@@ -147,11 +217,104 @@ mod tests {
             f_point.push(Fr::zero());
             assert_eq!(p, f.evaluate(&f_point).unwrap());
 
-            let mut f_point = vec![Fr::zero()];
-            let v_point: Vec<_> = (0..s).map(|_| Fr::rand(&mut rng)).collect();
-            f_point.extend(&v_point);
             // assert v(r) = f(0, r)
-            assert_eq!(v.evaluate(&v_point).unwrap(), f.evaluate(&f_point).unwrap());
+            for _ in 0..50 {
+                let mut f_point = vec![Fr::zero()];
+                let v_point: Vec<_> = (0..s).map(|_| Fr::rand(&mut rng)).collect();
+                f_point.extend(&v_point);
+                assert_eq!(v.evaluate(&v_point).unwrap(), f.evaluate(&f_point).unwrap());
+            }
+
+            // assert f(1, x) = f(x, 0) * f(x, 1)
+            for _ in 0..50 {
+                // let x: Vec<_> = (0..s).map(|_| Fr::rand(&mut rng)).collect();
+                let x: Vec<_> = (0..s)
+                    .map(|_| {
+                        let b = bool::rand(&mut rng);
+                        Fr::from(b as u8)
+                    })
+                    .collect();
+
+                // f(1, x)
+                let mut f_point = vec![Fr::one()];
+                f_point.extend(&x);
+
+                // f(x, 0)
+                let mut f_left = x.clone();
+                f_left.push(Fr::zero());
+                // f(x, 1)
+                let mut f_right = x.clone();
+                f_right.push(Fr::one());
+
+                assert_eq!(
+                    f.evaluate(&f_point).unwrap(),
+                    (f.evaluate(&f_left).unwrap() * f.evaluate(&f_right).unwrap())
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn mle_eq() {
+        let mut rng = test_rng();
+        for s in 0..=10 {
+            println!("s: {}", s);
+
+            for _ in 0..50 {
+                let bool_point: Vec<_> = (0..s)
+                    .map(|_| {
+                        let b = bool::rand(&mut rng);
+                        Fr::from(b as u8)
+                    })
+                    .collect();
+                let other_point: Vec<_> = (0..s)
+                    .map(|_| {
+                        let b = bool::rand(&mut rng);
+                        Fr::from(b as u8)
+                    })
+                    .collect();
+                if bool_point == other_point {
+                    println!("ups, points were equal");
+                    continue;
+                }
+                let eq = compute_mle_eq::<Bls12_381>(&bool_point, s);
+                assert_eq!(eq.evaluate(&bool_point).unwrap(), Fr::one());
+                assert_eq!(eq.evaluate(&other_point).unwrap(), Fr::zero());
+            }
+        }
+    }
+    #[test]
+    fn g_computed_correctly() {
+        let mut rng = test_rng();
+        for s in 4..=4 {
+            println!("s: {}", s);
+            let v = DenseMultilinearExtension::<Fr>::rand(s, &mut rng);
+            let (f, _p) = compute_f::<Bls12_381>(&v);
+
+            let bool_point: Vec<_> = (0..s)
+                .map(|_| {
+                    let b = bool::rand(&mut rng);
+                    Fr::from(b as u8)
+                })
+                .collect();
+            println!("bool_point: {:?}", bool_point);
+            let g = compute_G::<Bls12_381>(&f, &bool_point);
+            assert_eq!(g.evaluate(&bool_point).unwrap(), Fr::zero());
+
+            let sum = (0..(1 << s)).fold(Fr::zero(), |acc, b| {
+                let v_point: Vec<Fr> = (0..s)
+                    .map(|j| {
+                        if (b >> j) & 1 == 1 {
+                            Fr::one()
+                        } else {
+                            Fr::zero()
+                        }
+                    })
+                    .collect();
+                println!("v_point: {:?}", v_point);
+                acc + g.evaluate(&v_point).unwrap()
+            });
+            assert_eq!(sum, Fr::zero());
         }
     }
 }
